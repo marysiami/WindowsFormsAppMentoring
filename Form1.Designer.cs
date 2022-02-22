9080df7551281ef21ddd6de87864dd53c0c50286
EPAM\Maria_Zimnoch <maria_zimnoch@epam.com>
2022-02-22 09:42:09 +0100

    cleaning

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -62,7 +62,6 @@
             this.label1.Size = new System.Drawing.Size(77, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Your name is...";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
