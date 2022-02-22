77521c42be164def6c8d060ba25f6a663b224559
EPAM\Maria_Zimnoch <maria_zimnoch@epam.com>
2022-02-22 09:23:12 +0100

    Add project files.

---
+++ BuisnessLogic/Greeting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public static class Greeting
+    {
+        public static string Welcome(string? name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                var date = DateTime.Now;
+                return $"{date.ToShortTimeString()} {date.ToShortDateString()}\n\nHello, {name}!";
+            }
+
+            return "There is no name.";
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using BusinessLogic;
+
+namespace WindowsFormsAppMentoring
+{
+    public partial class Form1 : Form
+    {
+        public string UserName { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            UserName = textBox1.Text;
+        }
+        private void buttonHelloWorld_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = Greeting.Welcome(UserName);            
+        }
+
+    }
+}
